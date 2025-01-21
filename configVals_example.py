0548faede45dc4a06649334c494a03a578d2bec7
Ben Thurber <bgt26@uclive.ac.nz>
2025-01-21 21:19:07 +1300

    Simplified code to just IMAP listener functionality. Replaced pop3 with imap.

--- configVals_example.py
+++ configVals_example.py
@@ -5,14 +5,6 @@ POP3_SERVER = "pop3.example.com"
 IMAP_SERVER = "imap.example.com"
 EMAIL_USER = "username"
 EMAIL_PASSWORD = "password"
-IFTTT_WEBHOOK_URLS = "https://maker.ifttt.com/trigger/.....|https://maker.ifttt.com/trigger/....."
-IFTTT_WEBHOOK_ADMIN_URLS = "https://maker.ifttt.com/trigger/.....|https://maker.ifttt.com/trigger/....."
-
-# --- SETUP ---
-IMPORTANT_EMAIL_SENDERS = "important.sender@example.com|another.sender@example.com"
-IMPORTANT_EMAIL_SUBJECTS = "urgent|hurry|fast"  # Checks if subject contains these as sub-text, case-insensitive
 
 # --- SETTINGS ---
-EMAIL_SEARCH_DEPTH = "100"  # No. of max newest emails to check, before stopping
-IFTTT_NOTIFICATIONS_LIMIT = "5"  # No. of max notifications sent, before stopping
-SEND_TEST_NOTIFICATION = '0'  # 1 or 0
+EMAIL_SEARCH_DEPTH = "20"  # No. of max newest emails to check, before stopping
--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 # coding=utf-8
-from __future__ import unicode_literals
-import poplib
+import io
+import logging
 import email
 import email.parser
 import email.header
-import dateutil.parser
-import requests
+import socket
 import imaplib2
-import os
 import threading
 import sys
-import codecs
-
 import time
-
 import signal
+from utilities import config_logging
 
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+config_logging("output.log")
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 
 class _Config:
@@ -26,43 +24,20 @@ class _Config:
     EMAIL_USER = None
     EMAIL_PASSWORD = None
     EMAIL_SEARCH_DEPTH = None
-    IMPORTANT_EMAIL_SENDERS = None
-    IMPORTANT_EMAIL_SUBJECTS = None
-    IFTTT_WEBHOOK_URLS = None
-    IFTTT_NOTIFICATIONS_LIMIT = None
-    IFTTT_WEBHOOK_ADMIN_URLS = None
-    SEND_TEST_NOTIFICATION = None
 
     def __init__(self):
         for key in [a for a in dir(self) if not a.startswith('__') and not callable(getattr(self, a))]:
-            if 'HEROKU' in os.environ:
-                setattr(self, key, os.environ[key])
-            else:
-                import configVals_example
-                getattr(configVals_example, key)  # Only to help to remember to update the example config file
+            import configVals_example
+            getattr(configVals_example, key)  # Only to help remember to update the example config file
 
-                import configVals
-                setattr(self, key, getattr(configVals, key))
+            import configVals
+            setattr(self, key, getattr(configVals, key))
 
 
 config = _Config()
 
-PING_MAGIC_SUBJECT = 'X4p7QxyZZ3HTogT2bUBDz0Ci81ZfRbae5MirVZZbPLuqAB8sFtgOthLTZCLn3dxkutOgGY'
 prevEmailTimestamp = "Sat, 01 Jan 2000 00:00:00 +0000"
 prevEmailTimestampTempNew = None
-
-
-def sendNotification(title='', text='', txtPrefix='Notification', urlsString=config.IFTTT_WEBHOOK_URLS):
-    urls = urlsString.split('|')
-    deliveryStatuses = []
-    for url in urls:
-        r = requests.post(url, data={'value1': title, 'value2': text, })
-        deliveryStatuses.append('{} {}'.format(r.status_code, r.reason))
-    print('{} sent [{}]: {} | {}'.format(txtPrefix, ', '.join(deliveryStatuses), title, text))
-
-
-def sendAdminNotificationAndPrint(title='', text=''):
-    sendNotification(title=title, text=text, txtPrefix='Admin Notif.', urlsString=config.IFTTT_WEBHOOK_ADMIN_URLS)
 
 
 def decodeMimeText(s):
@@ -72,79 +47,32 @@ def decodeMimeText(s):
         for m in mimeTextEncodingTuples)
 
 
-def searchNewestEmail(notificationLimit=int(config.IFTTT_NOTIFICATIONS_LIMIT), sendOnlyTestNotif=False):
+def searchNewestEmail():
     global prevEmailTimestamp, prevEmailTimestampTempNew
-    server = poplib.POP3(config.POP3_SERVER)
-    server.user(config.EMAIL_USER)
-    server.pass_(config.EMAIL_PASSWORD)
+    try:
+        mail = imaplib2.IMAP4_SSL(config.IMAP_SERVER)
+        mail.login(config.EMAIL_USER, config.EMAIL_PASSWORD)
+        mail.select("inbox")
 
-    # list items on server
-    resp, items, octets = server.list()
+        result, data = mail.search(None, "ALL")
+        email_ids = data[0].split()
+        searchLimit = int(config.EMAIL_SEARCH_DEPTH)
 
-    L = len(items)
-    searchLimit = int(config.EMAIL_SEARCH_DEPTH)
+        for i in reversed(email_ids[-searchLimit:]):
+            result, msg_data = mail.fetch(i, "(RFC822)")
+            raw_email = msg_data[0][1].decode("utf-8")
+            message = email.message_from_string(raw_email)
 
-    sentNotifications = 0
-    for i in reversed(range(max(0, L - searchLimit), L)):
-        s = items[i].decode("utf-8")
-        id, size = s.split(' ')
-        resp, text, octets = server.top(id, 0)
-        # because server.retr(id) trips seen flag, server.top(...) doesn't,
-        # and also (POSSIBLY?) double-triggers event (first - message received, second - a message is read)
-        # NOTE: .top(...) is poorly specified in RFC, therefore might be buggy depending on server
+            subject = decodeMimeText(message["Subject"])
+            sender = decodeMimeText(message["From"])
+            logging.info(f"<{sender}> {subject}")
 
-        text = '\n'.join(t.decode("ascii", 'ignore') for t in text)
+        if prevEmailTimestampTempNew is not None:
+            prevEmailTimestamp = prevEmailTimestampTempNew
+            prevEmailTimestampTempNew = None
 
-        message = email.message_from_string(text)
-
-        d = dict(message.items())
-
-        subject = decodeMimeText(d['Subject'])
-        sender = decodeMimeText(d['From'])
-
-        isImportantSender = any(
-            importantEmailSender in sender for importantEmailSender in config.IMPORTANT_EMAIL_SENDERS.split('|'))
-        isImportantSubject = any(
-            importantSubject in subject.lower() for importantSubject in
-            config.IMPORTANT_EMAIL_SUBJECTS.lower().split('|'))
-
-        # Stupid way to live-test listener condition in pre-prod after recovery from errors
-        if subject == PING_MAGIC_SUBJECT and isImportantSender:
-            sendAdminNotificationAndPrint("Ping email found", "Pong.")
-            continue
-
-        if (isImportantSender or isImportantSubject):
-            newEmailTimestamp = d['Date']
-            newEmailDate = dateutil.parser.parse(newEmailTimestamp)
-            prevEmailDate = dateutil.parser.parse(prevEmailTimestamp)
-            if newEmailDate > prevEmailDate:
-                if prevEmailTimestampTempNew is None:
-                    prevEmailTimestampTempNew = newEmailTimestamp
-
-                if sendOnlyTestNotif:
-                    sendNotification('Email notifier STARTED!',
-                                     'EXAMPLE EMAIL: ' + subject + ', ' + sender + ', ' + newEmailTimestamp)
-                    break
-
-                if sentNotifications < notificationLimit:
-                    sendNotification(subject, sender + ', "' + newEmailTimestamp + '", (' + subject + ')')
-                    sentNotifications += 1
-                    if sentNotifications >= notificationLimit:
-                        print('END: Further search stopped due to reached notification limit of {}'
-                              .format(notificationLimit))
-                        break
-                elif notificationLimit == 0:
-                    break
-            else:
-                if sentNotifications == 0:
-                    print('NO-OP: no new important emails since "{}"'.format(prevEmailTimestamp))
-                else:
-                    print(
-                        'END: Further search stopped due to a depth limit of "{}"'.format(prevEmailTimestamp))
-                break
-    if prevEmailTimestampTempNew is not None:
-        prevEmailTimestamp = prevEmailTimestampTempNew
-        prevEmailTimestampTempNew = None
+    except imaplib2.IMAP4.error as e:
+        logging.error(f"Failed to connect to IMAP server: {e}")
 
 
 # This is the threading object that does all the waiting on
@@ -173,14 +101,14 @@ class IMAPClientManager(object):
         while True:
             # This is part of the trick to make the loop stop
             # when the stop() command is given
-            if self.event.isSet():
+            if self.event.is_set():
                 return
             self.needsync = False
 
             # A callback method that gets called when a new
             # email arrives. Very basic, but that's good.
             def callback(args):
-                if not self.event.isSet():
+                if not self.event.is_set():
                     self.needsync = True
                     self.event.set()
 
@@ -225,7 +153,7 @@ class GracefulKiller:
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
-        print("Caught kill signal: {}".format(signum))
+        logging.info("Caught kill signal: {}".format(signum))
         self.kill_now = True
 
 
@@ -233,7 +161,6 @@ imapClientManager = None
 imapClient = None
 killer = GracefulKiller()
 
-_sendTestNotification = bool(int(config.SEND_TEST_NOTIFICATION))
 while True:
     try:
         try:
@@ -242,32 +169,33 @@ while True:
             imapClient.select("INBOX")  # We need to get out of the AUTH state, so we just select the INBOX.
             imapClientManager = IMAPClientManager(imapClient)  # Start the Idler thread
             imapClientManager.start()
-            print('IMAP listening has started')
+            logging.info('IMAP listening has started')
 
             # Helps update the timestamp, so that on event only new emails are sent with notifications
-            searchNewestEmail(notificationLimit=0, sendOnlyTestNotif=_sendTestNotification)
-            _sendTestNotification = False
+            searchNewestEmail()
 
-            while not killer.kill_now and not imapClientManager.needsReset.isSet():
+            while not killer.kill_now and not imapClientManager.needsReset.is_set():
                 time.sleep(1)
 
-            if imapClientManager.needsReset.isSet():
+            if imapClientManager.needsReset.is_set():
                 raise imapClientManager.needsResetExc  # raises instance of imaplib2.IMAP4.abort
             elif killer.kill_now:
                 break
         finally:
             if imapClientManager is not None:
-                imapClientManager.stop()  # Had to do this stuff in a try-finally, since some testing went a little wrong..
+                imapClientManager.stop()  # Had to do this stuff in a try-finally, since some testing went a little wrong.
                 imapClientManager.join()
             if imapClient is not None:
                 imapClient.close()
                 imapClient.logout()  # This is important!
-            print('IMAP listening has stopped, conn cleanup was run for: Listener: {}, Client: {}'
+            logging.info('IMAP listening has stopped, conn cleanup was run for: Listener: {}, Client: {}'
                   .format(imapClientManager is not None, imapClient is not None))
             sys.stdout.flush()  # probably not needed
     except imaplib2.IMAP4.abort as e:
         retryDelay_s = 1
-        sendAdminNotificationAndPrint("Conn error, re {}s".format(retryDelay_s), str(e))
         sleepUnless(retryDelay_s, lambda: killer.kill_now)
         if killer.kill_now:
             break
+    except socket.gaierror as e:
+        logging.error(f"Failed to connect to IMAP server: {e}")
+        break
